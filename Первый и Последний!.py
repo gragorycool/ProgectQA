c4a79d750208af07d677df1dac61dcf3283cb902
gragorycool <gragorycool86@gmail.com>
2021-01-20 14:02:38 +0200

    first and last

---
+++ Первый и Последний!.py
@@ -0,0 +1,9 @@
+def my_func(my_list):
+    for x in my_list:
+        print(x)
+        print("первое число:", x[0])
+        print("последнее число:", x[-1])
+
+
+my_list = [input()]
+my_func(my_list)
